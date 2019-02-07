5c03047734efda3dcffdae43a60f4b3d39cb109b
Guest User <Guest@macos-20.local>
2019-02-07 16:45:03 -0500

    Initial Commit

---
+++ MyTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTableView
+//
+//  Created by Guest User on 2019-02-07.
+//  Copyright © 2019 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
